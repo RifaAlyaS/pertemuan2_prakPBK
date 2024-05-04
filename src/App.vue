d8f1f5b7934b787ecd3860d5ddcf39857d2a6369
RifaAlyaS <rifaalyashadrina@student.uir.ac.id>
2024-05-04 21:28:59 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input type="text" v-model="inputText" placeholder="Masukkan teks yang ingin diubah">
+    <button @click="toggleColor">Ubah Warna</button>
+    <div :class="{ 'purple': iniWarna }" :style="{ 'font-size': fontSize + 'px' }">
+      {{ inputText }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputText: '', // Teks input yang dimasukkan oleh pengguna
+      iniWarna: false,
+      fontSize: 16
+    };
+  },
+  methods: {
+    toggleColor() {
+      this.iniWarna = !this.iniWarna;
+      this.fontSize += 2;
+    }
+  }
+};
+</script>
+
+<style>
+.purple{
+  color: rgb(207, 0, 173);
+}
+</style>
